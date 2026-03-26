f881e0d02ae26e9114d5de25ae87e26a226509d6
samad0502 <abechaa@ymail.com>
2026-03-26 08:20:08 +0100

    add filter controller for menus

---
+++ app/Controllers/ApiController.php
@@ -0,0 +1,27 @@
+<?php
+require_once ROOT . 'app/Models/Menu.php';
+
+class ApiController {
+    public function getFiltredMenus() {
+        $database = new Database();
+        $db = $database->getConnection();
+        $menuModel = new Menu($db);
+
+        //recup des filtres envoyés par le JS (GET)
+        $filters = [
+            'priceMin' => $_GET['priceMin'] ?? null,
+            'priceMax' => $_GET['priceMax'] ?? null,
+            'minPeople' => $_GET['minPeople'] ?? null,
+            'theme' => $_GET['theme'] ?? null,
+            'diet' => $_GET['diet'] ?? null
+        ];
+
+        $menus = $menuModel->getFiltred($filters);
+
+        //reponse en json
+        header('Content-type: application/json');
+        echo json_encode($menus);
+        exit;
+
+    }
+}
