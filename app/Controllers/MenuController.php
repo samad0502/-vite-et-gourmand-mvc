65a7bbea2db36f72e036998fb865085c89210172
samad0502 <abechaa@ymail.com>
2026-03-27 10:40:57 +0100

    Added the `detail()` function which links the ID in the URL to the view in the MenuController.

--- app/Controllers/MenuController.php
+++ app/Controllers/MenuController.php
@@ -10,5 +10,26 @@ class MenuController {
         $diets = $menuModel->getUniqueValues('diet');
 
         require_once ROOT . 'app/Views/menus.php';
+
         }
-}
+
+    public function detail() {
+        $id = isset($_GET['id']) ? (int)$_GET['id'] : 0;
+
+        $menuModel = new Menu();
+        $menu = $menuModel->getMenuById($id);
+ 
+        // si le menus n'existe pas  on retourne a la liste
+        if(!$menu){
+            header('Location: index.php?page=menus');
+            exit;
+        }
+     // on definit si l'utilisateur est connecter pour lz bouton panier
+     $isLogges = isset($_SESSION['user']);
+
+     require_once ROOT . 'app/Views/menu_detail.php';
+
+     }
+
+
+     }
