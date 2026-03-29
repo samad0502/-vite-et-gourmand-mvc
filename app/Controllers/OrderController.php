f0cbe15db745645bb9117b5d006dd121dc39134a
samad0502 <abechaa@ymail.com>
2026-03-29 12:38:58 +0200

    Added final processing function for the order (process()

--- app/Controllers/OrderController.php
+++ app/Controllers/OrderController.php
@@ -15,4 +15,43 @@ public function checkout() {
 
  require_once ROOT . 'app/Views/orders/checkout.php';   
 }
+
+//traitement final de la commande
+public function process() {
+    $db = (new Database())->getConnection();
+    try {
+        $db->beginTransaction();
+        $orderModel = new Order();
+        $groupOrderNumber = 'ORD-' . strtoupper(uniqid());
+
+        foreach($_SESSION['cart'] as $item){
+//Logique de calcul prix/promo/livraison
+            $orderData = [
+                'order_number' => $groupOrderNumber,
+                'user_id' => $_SESSION['user']['id'],
+                'menu_id' => $item['menu_id'],
+                'number_people' => $item['number_people'],
+                'equipment_ready' => $item['equipment_ready'],
+                'address' => $_POST['address'],
+                'delivery_date' => $_POST['delivery_date'],
+                'delivery_time' => $_POST['delivery_time'],
+                'total_price' => $_POST['final_total_price']
+            ];
+
+            $orderModel->createOrder($orderData);
+
+            //mise a jour du stock
+            $db->prepare("UPDATE menus SET remaining_quantity = remaining_quantity - 1 WHERE id = ?")
+            ->execute([$item['menu_id']]);
+        }
+
+        $db->commit();
+        unset($_SESSION['cart']);
+        header('Location: index.php?page=order_success$ref=' . $groupOrderNumber);
+
+    } catch(Exception $e){
+        $db->rollBack();
+        die("Erreur : " . $e->getMessage());
+    }
+}
 }
