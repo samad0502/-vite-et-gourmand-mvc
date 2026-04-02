dcc8e599afc1d7836f05d6cf834ea9a682085d9f
samad0502 <abechaa@ymail.com>
2026-04-02 17:04:23 +0200

    Typo corrections & added dashboard display function and status change processing (updateOrderStatus())

--- app/Controllers/EmplyeeController.php
+++ app/Controllers/EmplyeeController.php
@@ -1,21 +1,39 @@
 <?php
 
-class AdminController {
+class EmplyeeController {
 
 public function dashboard() {
     if($_SESSION['user']['role'] !== 'employee'&& $_SESSION['user']['role'] !== 'admin'){
-        header('Location: index.php');
+        header('Location: index.php?page=login');
         exit;
     }
     
-    // logique de filtrage 
-    $statusFilter = $_GET['status'] ?? '';
-    $search = $_GET['client'] ?? '';
+  
+    
     
     $orderModel = new Order();
-    $orders = $orderModel->getFilteredOrders($statusFilter, $search);
+    $statusFilter = $_GET['status'] ?? '';
+    $searchClient = $_GET['client'] ?? '';
+    $orders = $orderModel->getOrdersForEmployee($statusFilter, $searchClient);
     
   
     require_once ROOT . 'app/Views/employee/dashboard.php';
+}
+
+
+public function updateOrderStatus() {
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $orderId = (int)$_POST['order_id'];
+        $newStatus = $_POST['new_status'];
+
+        $orderModel = new Order();
+        if($orderModel->updateStatus($orderId, $newStatus)) {
+            header('Location: index.php?page=employee_dashboard&success=status_updated');
+        } else {
+            header('Location: index.php?page=employee_dashboard&error=update_failed');
+
+        }
+        exit;
+    }
 }
 }
