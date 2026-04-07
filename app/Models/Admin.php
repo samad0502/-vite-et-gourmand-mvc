8d9629e437fb082083bac24f09aaa8956bbbb84f
samad0502 <abechaa@ymail.com>
2026-04-07 17:10:24 +0200

    Added function for statistics (NoSQL)

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -26,4 +26,28 @@ class Admin {
         $stmt = $this->db->prepare("UPDATE users SET is_active = NOT is_active WHERE id = ?");
         return $stmt->execute([$userId]);
     }
+
+    
+    // statistiques nosql mongodb
+    public function getMongoStats($menusFilter = '', $dateStart = '', $dateEnd = '' ) {
+      $user = "prof_correction";
+      $pass = urlencode("ViteGourmand2026");
+      $cluster = "cluster0.ziybmvg.mongodb.net";
+      $dbname = "vite_gourmand";
+      $uri = "mongodb+srv://$user:$pass@$cluster/$dbname?retryWrites=true&w=majority";
+      
+      $client = new MongoDB\Client($uri);
+      $collection = $client->$dbname->order_stats;
+
+      $filter = [];
+      if($menusFilter) $filter['menu_name'] = $menusFilter;
+
+      if($dateStart || $dateEnd) {
+        $dateRange = [];
+        if($dateStart)$dateRange['$gte'] = new MongoDB\BSON\UTCDateTime(strtotime($dateStart) * 1000);
+        if($dateEnd) $dateRange['$lte'] = new MongoDB\BSON\UTCDateTime(strtotime($dateEnd . ' +1 day') * 1000);
+        $filter['executed_at'] = $dateRange;
+        }
+        return $collection->find($filter);
+    }
 }
