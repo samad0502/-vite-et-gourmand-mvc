43e2bb6c7e09161312936978b3bf071da10fe2f1
samad0502 <abechaa@ymail.com>
2026-04-08 18:29:34 +0200

    page division (user, stats, employee, etc.) and addition of users() and stats() functions

--- app/Controllers/AdminController.php
+++ app/Controllers/AdminController.php
@@ -9,12 +9,40 @@ class AdminController {
         }
 
         $db = (new Database())->getConnection();
+        $stmt = $db->query("SELECT o.*, u.firstname, m.title as menu_name
+                            FROM orders o
+                            JOIN users u ON o.user_id = u.id
+                            JOIN menus m ON o.menu_id = m.id
+                            WHERE o.order_status NOT IN ('finished', 'cancelled'");
+        $orders = $stmt->fetchAll(PDO::FETCH_ASSOC);    
+        
+        require_once ROOT . 'app/Views/admin/dashboard.php';
+    }
+
+
+    //gestion des utilisateurs
+    public function users() {
+        if($_SESSION['user']['role'] !== 'admin') {
+            header('Location: index.php?page=login');
+            exit;
+}
+        $db = (new Database())->getConnection();
         $adminModel = new Admin($db);
-        $orderModel = new Order();
-
-        // 1. Données pour la gestion des employés
         $employees = $adminModel->getAllEmployees();
+        
+        require_once ROOT . 'app/Views/admin/users.php';
+    }
 
+
+    //statistiques
+    public function stats() {
+        if($_SESSION['user']['role'] !== 'admin'){
+            header('Location: index.php?page=login');
+            exit;
+        }
+
+        $db = (new Database())->getConnection();
+        $adminModel = new Admin($db);
         //données pour les stats
         $statsData = $adminModel->getMongoStats($_GET['menu'] ?? '', $_GET['start'] ?? '', $_GET['end'] ?? '');
 
@@ -26,13 +54,13 @@ class AdminController {
             $menuStats[$name] = ($menuStats[$name] ?? 0 ) + 1 ;
             $totalCA += $doc['price'];
         }
+
+        require_once ROOT . 'app/Views/admin/stats.php';
        
-        require_once ROOT . 'app/Views/admin/dashboard.php';
-
         }
+    
 
-
-        public function createEmployee() {
+            public function createEmployee() {
             if($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $db =(new Database())->getConnection();
                 $email = $_POST['email'];
@@ -40,7 +68,7 @@ class AdminController {
             
                 try {
                 //insertion role_id(employé ou 2)
-                $stmt = $db->prepare("INSERT INTO users (firstname, lastname, email, password, role_id, is_active) VALUES(?, ?, ?, ?, 2, 1");
+                $stmt = $db->prepare("INSERT INTO users (firstname, lastname, email, password, role_id, is_active) VALUES(?, ?, ?, ?, 2, 1)");
                 $stmt->execute([$_POST['firstname'], $_POST['lastname'], $email, $password]);
 
                 //envoi du mail d'inscription employé (sans mdp)
