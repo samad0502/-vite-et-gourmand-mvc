955495c06439a8b723fc8ea14c4d152e3ae8e0cc
samad0502 <abechaa@ymail.com>
2026-04-20 03:27:14 +0200

    retrieving all user/mode functions for user/repo

---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,72 @@
+<?php
+
+class UserRepository {
+    private $db;
+
+    public function __construct($db)
+    {
+        $this->db = $db;
+    }
+
+    public function findById($id) {
+    $sql = "SELECT id, firstname, lastname, email, phone, address, zip_code, city
+    FROM users
+    WHERE id = ?";
+
+    $stmt = $this->db->prepare($sql);
+    $stmt->execute([$id]);
+    return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+
+     public function login($email, $password){
+        $sql = "SELECT u.*, r.name as role_name
+        FROM users u
+        JOIN roles r ON u.role_id = r.id
+        WHERE u.email = :email AND u.is_active = 1 ";
+
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute(['email' => $email]);
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if($user && password_verify($password, $user['password'])){
+            return $user;
+        }
+        return false;
+
+    }
+
+
+     public function update($id, $data) {
+        $sql = "UPDATE users SET
+                firstname = ?,
+                lastname = ?,
+                phone = ?,
+                address = ?
+                WHERE id = ?";
+
+       $stmt = $this->db->prepare($sql);
+       return $stmt->execute([
+        $data['firstname'],
+        $data['lastname'],
+        $data['phone'],
+        $data['address'],
+        $id
+       ]);         
+    }
+
+
+    public function register(array $data): bool {
+        $passwordHash = password_hash($data['password'], PASSWORD_DEFAULT);
+        $stmt = $this->db->prepare("
+        INSERT INTO users
+        (firstname, lastname, address, city, zip_code, phone, email, password, role_id, is_active)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?) ");
+
+        return $stmt->execute([
+            $data['firstname'], $data['lastname'], $data['address'],
+            $data['city'], $data['zip_code'], $data['phone'],
+            $data['email'], $passwordHash, $data['role_id'] ?? 3, 1
+        ]);
+    }
+}
